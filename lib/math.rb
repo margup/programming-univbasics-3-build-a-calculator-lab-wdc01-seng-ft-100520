8464d71861ee903ae74f18f576dc2cc63e38d771
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-12 20:14:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,12 +3,12 @@ num1 + num2
 end
 
 def subtraction(num1, num2)
-  num2 - num1
+  num1 - num2
 
 end
 
 def division(num1, num2)
-  num1 * num2
+  num2 * num1
 
 end
 
@@ -18,7 +18,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  num2 % num1
+  num1 % num2
 
 end
 
